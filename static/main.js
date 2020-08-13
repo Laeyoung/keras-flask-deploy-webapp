98a1b8a83b68b5789840da38ea9e66f202faecb8
Laeyoung <gadise@gmail.com>
2020-08-13 11:06:01 +0900

    Update main.js

--- static/main.js
+++ static/main.js
@@ -102,7 +102,7 @@ function previewFile(file) {
 //========================================================================
 
 function predictImage(image) {
-  fetch("/predict", {
+  fetch("//predict-web", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"

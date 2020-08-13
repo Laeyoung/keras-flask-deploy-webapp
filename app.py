c0e92b2203a95d38be885ea81faa185131c6b9eb
Laeyoung <gadise@gmail.com>
2020-08-13 11:05:28 +0900

    Update app.py

--- app.py
+++ app.py
@@ -92,6 +92,31 @@ def predict():
     return None
 
 
+@app.route('/predict-web', methods=['GET', 'POST'])
+def predict():
+    if request.method == 'POST':
+        # Get the image from post request
+        img = base64_to_pil(request.json)
+
+        # Save the image to ./uploads
+        # img.save("./uploads/image.png")
+
+        # Make prediction
+        preds = model_predict(img, model)
+
+        # Process your result for human
+        pred_proba = "{:.3f}".format(np.amax(preds))    # Max probability
+        pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
+
+        result = str(pred_class[0][0][1])               # Convert to string
+        result = result.replace('_', ' ').capitalize()
+        
+        # Serialize the result, you can add additional fields
+        return jsonify(result=result, probability=pred_proba)
+
+    return None
+
+
 if __name__ == '__main__':
     # app.run(port=5002, threaded=False)
 
